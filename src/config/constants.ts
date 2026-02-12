e8822f3450b0e87db2955d0f17e110728130c42b
JoyKang210 <Joy.Kang@obigo.com>
2026-02-12 10:51:00 +0900

    refactor: r2이미지 경로 확장자 수정

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://pub-a45bc992c0594356a8d32a71510a246b.r2.dev";
-export const DEFAULT_IMAGE_FOLDER = "de_images/program";
+export const DEFAULT_IMAGE_FOLDER = "images/program";
 export const ITUNES_NS = "http://www.itunes.com/dtds/podcast-1.0.dtd";
